8f6e30e1b8636c121774929a20757ff1c797fba4
Mostafa Edrees <mostafaedrees018@gmail.com>
2026-05-07 18:16:53 +0300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -9,6 +9,8 @@ const rl = readline.createInterface({
 // TODO: Uncomment the code below to pass the first stage
 rl.prompt();
 
+let builtInCommands = ['echo', 'exit', 'type'];
+
 rl.on('line', (input) => {
 	if(input.includes(' ')) {
 		let [command, ...args] = input.split(' ');
@@ -18,7 +20,15 @@ rl.on('line', (input) => {
 			return;
 		}
 		
-		if(command === 'echo') {
+		if(isTypeCommand(command)) {
+			if(isBuiltInCommand(args[0])) {
+				console.log(`${args[0]} is a shell builtin`);
+			} else {
+				console.log(`${args[0]}: not found`);
+			}
+		}
+		
+		if(isEchoCommand(command)) {
 			console.log(...args);
 		}
 	} else {
@@ -33,3 +43,6 @@ rl.on('line', (input) => {
 })
 
 const isExitCommand = command => command === 'exit';
+const isEchoCommand = command => command === 'echo';
+const isTypeCommand = command => command === 'type';
+const isBuiltInCommand = command => builtInCommands.includes(command);
