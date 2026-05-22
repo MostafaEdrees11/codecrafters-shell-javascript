52fa8a7d43581f3d03274122d69be26288459a08
Mostafa Edrees <mostafaedrees018@gmail.com>
2026-05-22 15:27:05 +0300

    feat: handle -C flag in complete command
    - This commit adds support for the -C flag in the complete command, allowing users to specify a custom directory for command completion.
    The implementation includes parsing the -C flag and using the specified directory for generating completion suggestions.
    This enhancement improves the flexibility of the complete command and allows users to customize their command completion experience.

--- app/built-in-commands/complete.js
+++ app/built-in-commands/complete.js
@@ -1,10 +1,22 @@
+let registeredCommands = {};
+
 const complete = (command, args) => {
+    if (args.includes('-C')) {
+        let commandToRegister = args[args.indexOf('-C') + 2];
+        let pathToCompleter = args[args.indexOf('-C') + 1];
+        if (!registeredCommands.hasOwnProperty(commandToRegister)) {
+            registeredCommands = {
+                ...registeredCommands,
+                [commandToRegister]: pathToCompleter
+            };
+        }
+    }
 
     if (args.includes('-p')) {
-        let registeredCommands = [];
         let givenCommand = args[args.indexOf('-p') + 1];
 
-        if (registeredCommands.includes(givenCommand)) {
+        if (registeredCommands.hasOwnProperty(givenCommand)) {
+            process.stdout.write(`complete -C '${registeredCommands[givenCommand]}' ${givenCommand}\n`);
         } else {
             process.stdout.write(`complete: ${givenCommand}: no completion specification\n`)
         }
